ac4accaa769349f4517a26ddcae494ce68c09004
峰岸零 <minegishirei@minegishireinoMacBook-Air.local>
2023-09-27 16:49:31 +0900

    push

--- code/vue-notus/src/components/Cards/CardSocialTraffic2.vue
+++ code/vue-notus/src/components/Cards/CardSocialTraffic2.vue
@@ -8,11 +8,12 @@
           </h3>
         </div>
         <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-          <button
+          <a
             class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button">
+            type="button"
+            href="tables.html">
             See all
-          </button>
+          </a>
         </div>
       </div>
     </div>
